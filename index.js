11ea3a28d6182f908afd5d6e9c562bd2f64b9c57
jhon <jhonandres4360435@gmail.com>
2024-10-12 22:00:07 -0500

    node

--- index.js
+++ index.js
@@ -1,39 +1,10 @@
-const fs=require("fs")
-const express = require("express");
+const express= require("express");
 const app=express();
 const port=5000;
-app.use(express.static("public"))
-app.use(express.urlencoded({extended:true}))
+app.use(express.static(__dirname +  "/public"))
 app.get("/",(req,res)=>{
-         res.send("estamos en la raiz")
-})
-/*
-app.get("/formulario",(req,res)=>{
-        res.send("formulario enviado..")
-        console.log(req.query);
-        
-})
-*/
-app.post("/formulario",(req,res)=>{
-    
-
-     const {nombre,apellido}=req.body;
-
-     if(!nombre || !apellido) return res.redirect("/error.html")
-
-
-        fs.writeFile(`${nombre}.pdf`,apellido,(error)=>{
-            if(error) return res.send("fallo al crear el archivo")
-                res.send("se creo el archivo")
-        })
-  
+    res.send("solicitando ruta")
 })
 
-app.get("/mision",(req,res)=>{
-        res.send("estamos en mision")
-})
-
-app.listen(port,()=>{
-    console.log("servidor funcionando");
-    
-})
+app.listen(port,()=>console.log("servidor funcionando")
+)
